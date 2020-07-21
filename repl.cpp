f0065b5cd1ce795b66a10d7097f115a2fd791b0c
David Feinzimer <d@fein.io>
2020-07-21 00:32:19 -0700

    2020.07.21.00.32.17

--- repl.cpp
+++ repl.cpp
@@ -8,32 +8,27 @@ using namespace std;
 void analyze(string input) {
     const char per = '.';
     int count = 0;
-    for (int x = 0; x < input.length(); x++) {
-        if (input.at(x) ==  per) {
+    for(int x = 0; x < input.length(); x++) {
+        if(input.at(x) ==  per) {
             count ++;
         }
     }
     print("Statements Expected: " + to_string(count));
-    extractStatements(input);
+    vector<string> statements = extractStatements(input);
+    print("Statements Found: " + to_string(statements.size()));
 }
 
 vector<string> extractStatements(string inputString) {
     vector<string> result;
-    // print(inputString);
-    // int first = inputString.find('.');
-    // cout << first;
-    // // return result;
-    // while (first != -1) {
-    //     string newStatement = inputString.substr(0,first-1);
-    //     cout << newStatement;
-    //     result.push_back(newStatement);
-    //     inputString = inputString.substr(first, inputString.length());
-    //     first = inputString.find('.');
-    // }
-    // print("Statements Extracted: " + to_string(result.size()));
-    // for (int i = 0; i < result.size(); i++) {
-    //     cout << result[i] << endl;
-    // }
+    int start = 0;
+    for(int i = 0; i < inputString.length(); i++) {
+        if(inputString[i] == '.') {
+            string newStatement = inputString.substr(start, i);
+            result.push_back(newStatement);
+            start = i++;
+
+        }
+    }
     return result;
 }
 
